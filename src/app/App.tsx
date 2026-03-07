68dd9686b4fcfbeb3f58ce3282cad1c0e50bb05e
anas ahmed <ansalking150@gmail.com>
2026-03-07 23:32:51 +0200

    Remove Skills and Testimonials sections

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/sections/Hero';
 import { About } from './components/sections/About';
-import { Skills } from './components/sections/Skills';
 import { Projects } from './components/sections/Projects';
 import { Services } from './components/sections/Services';
-import { Testimonials } from './components/sections/Testimonials';
 import { Contact } from './components/sections/Contact';
 import { Footer } from './components/sections/Footer';
 import { LoadingSpinner } from './components/LoadingSpinner';
@@ -60,10 +58,8 @@ export default function App() {
             <main>
               <Hero />
               <About />
-              <Skills />
               <Projects />
               <Services />
-              <Testimonials />
               <Contact />
             </main>
             <Footer />
